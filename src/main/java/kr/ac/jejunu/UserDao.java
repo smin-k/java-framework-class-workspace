ca0927851912e7cb79ae583053f8b5f78de84948
tmdals8030 <a7965344@gmail.com>
2021-04-09 18:46:56 +0900

    template

--- src/main/java/kr/ac/jejunu/UserDao.java
+++ src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,34 +1,57 @@
 package kr.ac.jejunu;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
 import java.sql.*;
 
 public class UserDao {
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public User findById(Integer id) throws SQLException {
         //데이터 어딨어? => mysql
         String sql = "select * from  userinfo where id = ?";
         Object[] params = new Object[]{id};
-        return jdbcContext.findById(sql, params);
+        return jdbcTemplate.query(sql, rs -> {
+            User user = null;
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+            return user;
+        }, id);
     }
 
     public void insert(User user) throws SQLException {
         //데이터 어딨어? => mysql
         String sql = "insert into userinfo (name, password) values ( ?, ? )";
         Object[] params = new Object[]{user.getName(), user.getPassword()};
-        jdbcContext.insert(user, sql, params, this);
-
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(
+                    sql
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        user.setId(keyHolder.getKey().intValue());
     }
 
     public void update(User user) throws SQLException {
         //데이터 어딨어? => mysql
         String sql = "update userinfo set name = ?, password = ? where id = ?";
         Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
 
     }
 
@@ -36,9 +59,7 @@ public class UserDao {
         //데이터 어딨어? => mysql
         String sql = "delete from userinfo where id = ?";
         Object[] params = new Object[]{id};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
 
     }
-
-
 }
