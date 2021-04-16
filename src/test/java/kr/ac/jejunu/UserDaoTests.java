364bcb0fdfc96421d4ec5bd92391352a29d02e33
tmdals8030 <a7965344@gmail.com>
2021-04-16 17:43:56 +0900

    4/16-1

--- src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -2,8 +2,7 @@ package kr.ac.jejunu;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -16,39 +15,48 @@ public class UserDaoTests {
     static UserDao userDao;
 
     @BeforeAll
-    public static void setup() {
-        ApplicationContext applicationContext
-                = new AnnotationConfigApplicationContext(DaoFactory.class);
+    public static void setup() throws ClassNotFoundException {
+        ClassPathXmlApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("daoFactory.xml");
+//        StaticApplicationContext applicationContext =
+//                new StaticApplicationContext();
+//        BeanDefinition dataBeanDefinition = new RootBeanDefinition(SimpleDriverDataSource.class);
+//        dataBeanDefinition.getPropertyValues()
+//                .addPropertyValue("driverClass"
+//                        , Class.forName(System.getenv("DB_DRIVER")));
+//        dataBeanDefinition.getPropertyValues()
+//                .addPropertyValue("username"
+//                        , System.getenv("DB_USERNAME"));
+//        dataBeanDefinition.getPropertyValues()
+//                .addPropertyValue("password"
+//                        , System.getenv("DB_PASSWORD"));
+//        dataBeanDefinition.getPropertyValues()
+//                .addPropertyValue("url"
+//                        , System.getenv("DB_URL"));
+//
+//
+//
+//        applicationContext.registerBeanDefinition("dataSource", dataBeanDefinition);
+//        BeanDefinition jdbcBeanDefinition = new RootBeanDefinition(JdbcTemplate.class);
+//        jdbcBeanDefinition.getConstructorArgumentValues().addGenericArgumentValue(
+//                new RuntimeBeanReference("dataSource")
+//        );
+//        applicationContext.registerBeanDefinition("jdbcTemplate", jdbcBeanDefinition);
+//        BeanDefinition userDaoBeanDefinition = new RootBeanDefinition(UserDao.class);
+//        userDaoBeanDefinition.getConstructorArgumentValues().addGenericArgumentValue(
+//                new RuntimeBeanReference("jdbcTemplate")
+//        );
+//        applicationContext.registerBeanDefinition("userDao", userDaoBeanDefinition);
+//        ApplicationContext applicationContext
+//                = new AnnotationConfigApplicationContext(DaoFactory.class);
         userDao = applicationContext.getBean("userDao", UserDao.class);
-    }
-    // 의존성 컨테이너 컨트롤 도구?
-    @Test
-    public void update() throws SQLException, ClassNotFoundException {
-        String name = "smin";
-        String password = "5344";
-//        DaoFactory daoFactory = new DaoFactory();
-//        UserDao userDao = daoFactory.getUserDao();
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        userDao.insert(user);
-
-        user.setName("hulk");
-        user.setPassword("1234");
-
-        userDao.update(user);
-        User updatedUser = userDao.findById(user.getId());
-
-        assertThat(updatedUser.getId(), is(user.getId()));
-        assertThat(updatedUser.getName(), is(user.getName()));
-        assertThat(updatedUser.getPassword(), is(user.getPassword()));
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Integer id = 1;
-        String name = "smin";
-        String password = "5344";
+        String name = "hulk";
+        String password = "1234";
 //        DaoFactory daoFactory = new DaoFactory();
 //        UserDao userDao = daoFactory.getUserDao();
         User user = userDao.findById(id);
@@ -65,8 +73,6 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-//       DaoFactory daoFactory = new DaoFactory();
-//       UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
 
         User insertedUser = userDao.findById(user.getId());
@@ -76,8 +82,9 @@ public class UserDaoTests {
         assertThat(insertedUser.getName(), is(user.getName()));
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
+
     @Test
-    public void delete() throws SQLException, ClassNotFoundException {
+    public void update() throws SQLException {
         String name = "허윤호";
         String password = "1111";
 
@@ -85,12 +92,36 @@ public class UserDaoTests {
         user.setName(name);
         user.setPassword(password);
         userDao.insert(user);
-//       DaoFactory daoFactory = new DaoFactory();
-//        UserDao userDao = daoFactory.getUserDao();
-        userDao.delete(user.getId());
-        User deletededUser = userDao.findById(user.getId());
-        assertThat(deletededUser, is(nullValue()));
+
+        user.setName("hulk");
+        user.setPassword("1234");
+
+        userDao.update(user);
+
+        User updatedUser = userDao.findById(user.getId());
+
+        assertThat(updatedUser.getId(), is(user.getId()));
+        assertThat(updatedUser.getName(), is(user.getName()));
+        assertThat(updatedUser.getPassword(), is(user.getPassword()));
     }
+
+    @Test
+    public void delete() throws SQLException {
+        String name = "허윤호";
+        String password = "1111";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDao.insert(user);
+
+        userDao.delete(user.getId());
+
+        User deletedUser = userDao.findById(user.getId());
+
+        assertThat(deletedUser, nullValue());
+    }
+
 //
 //    @Test
 //    public void getHalla() throws SQLException, ClassNotFoundException {
@@ -124,4 +155,5 @@ public class UserDaoTests {
 //        assertThat(insertedUser.getPassword(), is(user.getPassword()));
 //    }
 //
+
 }
