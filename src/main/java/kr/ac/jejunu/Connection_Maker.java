8af78d72d5b55280d8705fe37811d98a7323a3d2
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-04-23 02:24:20 +0900

    quiz6

--- src/main/java/kr/ac/jejunu/Connection_Maker.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.jejunu;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface Connection_Maker {
-    public Connection getConnection() throws ClassNotFoundException,SQLException;
-}
