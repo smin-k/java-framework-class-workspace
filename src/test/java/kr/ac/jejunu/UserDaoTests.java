a5c64868cc8df8e51a5356a9a0d3081a88270372
tmdals8030 <48111893+tmdals8030@users.noreply.github.com>
2021-04-22 13:48:51 +0900

    week1

---
+++ src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -0,0 +1,39 @@
+package kr.ac.jejunu;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserDaoTests {
+    @Test
+    public void testGet() throws SQLException, ClassNotFoundException {
+        Integer id = 1;
+        String name = "hulk";
+        String password = "1234";
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        String name = "hulk";
+        String password = "1234";
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+        User Inserted_user = userDao.get(user.getId());
+        assertThat(Inserted_user.getName(), is(name));
+        assertThat(Inserted_user.getPassword(), is(password));
+    }
+
+}
