e018a0cba0525539331c801133d5bbf0c4199cdc
tmdals8030 <a7965344@gmail.com>
2021-04-23 10:56:58 +0900

    ques1

---
+++ src/main/java/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,51 @@
+package kr.ac.jejunu;
+
+import java.sql.*;
+
+public class UserDao {
+    public User get(Integer id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/smin?serverTimezone=UTC"
+                        , "root", "5344");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //리턴
+        return user;
+    }
+
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost/smin?serverTimezone=UTC"
+                        , "root", "5344");
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo (name,password) values (?,?)",Statement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setObject(1, user.getName());
+        preparedStatement.setObject(2, user.getPassword());
+        preparedStatement.executeUpdate();
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //리턴
+
+    }
+}
