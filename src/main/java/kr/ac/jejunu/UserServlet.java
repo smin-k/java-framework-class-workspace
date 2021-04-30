35798d1733a9ec0030261d4d4771c755778af9aa
tmdals8030 <a7965344@gmail.com>
2021-04-30 18:26:06 +0900

    add hello

--- src/main/java/kr/ac/jejunu/UserServlet.java
+++ src/main/java/kr/ac/jejunu/UserServlet.java
@@ -13,9 +13,20 @@ public class UserServlet extends GenericServlet {
     public void init(ServletConfig config) throws ServletException {
         System.out.println("*************** init *****************");
     }
-
+//
+//    @Override
+//    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+//        System.out.println("*************** service *****************");
+//    }
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         System.out.println("*************** service *****************");
+        StringBuffer response = new StringBuffer();
+        response.append("<html>");
+        response.append("<body>");
+        response.append("<h1>hello</h1>");
+        response.append("</body>");
+        response.append("</html>");
+        res.getWriter().println((response.toString()));
     }
 }
